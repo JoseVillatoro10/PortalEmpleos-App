e720f1b6ee1b885b102ee51480bdd04545850913
José Villatoro <villajo409@gmail.com>
2023-05-30 08:09:13 -0600

    Primer avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SolicitudesComponent } from './solicitudes/solicitudes.component';
+import { VacantesComponent } from './vacantes/vacantes.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { AcercaComponent } from './acerca/acerca.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SolicitudesComponent,
+    VacantesComponent,
+    CategoriasComponent,
+    UsuariosComponent,
+    AcercaComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes, { useHash: true }),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
